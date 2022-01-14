69b117e79a976e9d6fba4b0b5111e0fad5aad02a
SaicharanPapani <90519153+SaicharanPapani@users.noreply.github.com>
2022-01-14 16:41:25 -0700

    security

---
+++ src/main/java/com/saicharan/Inventory/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.saicharan.Inventory.config;
+
+import java.util.List;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+//	https://stackoverflow.com/questions/36968963/how-to-configure-cors-in-a-spring-boot-spring-security-application
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+//        corsConfiguration.setAllowCredentials(true);
+//        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        
+        // You can customize the following part based on your project, it's only a sample
+        http.authorizeRequests().antMatchers("/**").permitAll().anyRequest()
+                .authenticated().and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
+
+    }
+}
+
